5f72a0dd25c6a3408bc729eb7aa144df4d4a16f0
Aipeanut <zhouqiang1992@live.com>
2018-04-09 20:48:53 +0800

    test lib_buffer size

--- lib_buffer.c
+++ lib_buffer.c
@@ -4,7 +4,7 @@
 int main(int argc, char const *argv[])
 {
 	char buff[]="hello linux";
-	printf("%s\n",buff);
+	printf("%s\n",buff);// no \n  or  lib_buffer full 1024 bite
 	//write(1,buff,sizeof(buff));
 	//while(1);
 	return 0;
